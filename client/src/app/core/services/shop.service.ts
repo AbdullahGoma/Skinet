adf33a277c2b431fcb778ac1b38ff3ca8d87c29c
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2024-08-11 15:11:42 +0300

    Handle Pagination and Search

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -4,6 +4,7 @@ import { Pagination } from '../../shared/models/pagination';
 import { Product } from '../../shared/models/product';
 import { ProductBrand } from '../../shared/models/brand';
 import { ProductType } from '../../shared/models/type';
+import { ShopParams } from '../../shared/models/shopParams';
 
 @Injectable({
   providedIn: 'root'
@@ -17,22 +18,27 @@ export class ShopService {
   types: ProductType[] = [];
   brands: ProductBrand[] = [];
 
-  getProducts(brands?: string[], types?: string[], sort?: string) {
+  getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
 
-    if (brands && brands.length > 0) {
-      params = params.append('brands', brands.join(','));
+    if (shopParams.brands.length > 0) {
+      params = params.append('brands', shopParams.brands.join(','));
     }
 
-    if (types && types.length > 0) {
-      params = params.append('types', types.join(','));
+    if (shopParams.types.length > 0) {
+      params = params.append('types', shopParams.types.join(','));
     }
 
-    if (sort) {
-      params = params.append('sort', sort);
+    if (shopParams.sort) {
+      params = params.append('sort', shopParams.sort);
     }
 
-    params = params.append('pageSize', 10);
+    if (shopParams.search) {
+      params = params.append('search', shopParams.search);
+    }
+
+    params = params.append('pageSize', shopParams.pageSize);
+    params = params.append('pageIndex', shopParams.pageIndex);
 
     return this.http.get<Pagination<Product>>(this.baseUrl + 'products', {params});
   }
