cedc0e0aff1509722597cc590bf5d441980100f6
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2024-09-02 22:16:31 +0300

    Add Client side stripe, Creating the address element and Populating the address in the address form and handle angular lifetime

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7000/api/'
+    apiUrl: 'https://localhost:7000/api/',
+    stripePublicKey: 'pk_test_51PtvtzGQ81TNtVan664p1lDyeNae9djbvLDXDbw5DumhUZPqMy0aD2GO1UQUj17O25T1wX9l23zAD25EkcoydQK200WVfhxPSd',
+    
 };
