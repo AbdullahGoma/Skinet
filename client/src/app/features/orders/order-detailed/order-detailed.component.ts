9177c51a552e42b0415343e4d0606453431fa930
AbdullahGomaa <Abdullah.Goma.010170@gmail.com>
2024-09-16 23:26:31 +0300

    Creating order detailed page

--- client/src/app/features/orders/order-detailed/order-detailed.component.ts
+++ client/src/app/features/orders/order-detailed/order-detailed.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { OrderService } from '../../../core/services/order.service';
+import { ActivatedRoute } from '@angular/router';
+import { Order } from '../../../shared/models/order';
+import { MatCardModule } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-order-detailed',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButton,
+    DatePipe,
+    CurrencyPipe
+  ],
   templateUrl: './order-detailed.component.html',
   styleUrl: './order-detailed.component.scss'
 })
-export class OrderDetailedComponent {
+export class OrderDetailedComponent implements OnInit {
+  private orderService = inject(OrderService); // It injects the OrderService into the component without needing to explicitly declare it in the constructor.
+  private activatedRoute = inject(ActivatedRoute);
+  order?: Order;
 
+  // Angular lifecycle hook
+  ngOnInit(): void { // What should happen when a component is initialized.
+    this.loadOrder();
+  }
+
+  loadOrder() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (!id) return;
+    this.orderService.getOrderDetailed(+id).subscribe({ // Since getOrderDetailed likely returns an Observable, subscribe() is used to listen for the response.
+      next: order => this.order = order
+    });
+
+    // Observable: Lazy: starts the execution only when subscribe() is called.
+    // Promise: Eager: starts the execution as soon as created.
+  }
 }
